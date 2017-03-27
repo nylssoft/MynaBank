ef33fbe50628dc4825d4b0b641d64f13b741e38b
Niels Stockfleth <niels.stockfleth@opentext.com>
2017-03-27 08:38:21 +0200

    wpf application template

---
+++ Bank/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+/*
+    Myna Bank
+    Copyright (C) 2017 Niels Stockfleth
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Security;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace Bank
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Init();
+            }
+            catch (Exception ex)
+            {
+                HandleError(ex);
+                Close();
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+        }
+
+        // actions
+
+        private void Init()
+        {
+        }
+
+        private void HandleError(Exception ex)
+        {
+            MessageBox.Show(
+                this,
+                string.Format(Properties.Resources.ERROR_OCCURRED_0, ex.Message),
+                Title,
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+    }
+}
---
+++ Bank/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("MynaBank")]
+[assembly: AssemblyDescription("A bank account manager")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Myna Bank")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: NeutralResourcesLanguage("")]
+[assembly: Guid("508F852D-294F-4FDE-8B4E-3B146E326FBD")]
